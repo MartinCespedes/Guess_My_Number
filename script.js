c22ba2688c3859ea49887fb030fe507b6f8ccf56
MartinCespedes <martin.cespedes002@mymdc.net>
2023-02-17 16:52:36 -0500

    Added handling for invalid input and win condition

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const displayMessage = function (message) {
     document.querySelector('.message').textContent = message;
   };
   
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+
+    // When user wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  };
+
