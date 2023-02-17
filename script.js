be96ea1c9e3927a51cce98adc8d00b737921db55
MartinCespedes <martin.cespedes002@mymdc.net>
2023-02-17 16:46:19 -0500

    Added const displayMessage

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+  };
+  
